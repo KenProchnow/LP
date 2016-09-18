e81d26ca9e55a36b465924d6adea137559065f64
kenprochnow@gmail.com <kenprochnow@gmail.com>
2016-09-18 12:39:48 -0700

    added schemas

---
+++ schemas.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `leads` (
+  `auto_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(45) DEFAULT NULL,
+  `date_created` datetime DEFAULT NULL,
+  PRIMARY KEY (`auto_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
